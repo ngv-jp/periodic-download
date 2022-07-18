30694879562e381a037e8070b74a0e894d8960d6
Ryota Funaki <ryota.funaki@ngv.jp>
2022-07-19 08:47:04 +0900

    Change use log functions and bugfix

--- bin/download.sh
+++ bin/download.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 # Copyright (c) 2022 RFull Development
 # This source code is managed under the MIT license. See LICENSE in the project root.
+SHELL_PATH="$0"
+SHELL_DIR=`dirname "$0"`
+PATH=$PATH:$SHELL_DIR
+
+cd "$SHELL_DIR"
+. ./log.sh
+
 load_params() {
   local OPTS REQUIRE_OPTS TEMP
 
-  # オプションを解析
+  # Option analysis
   OPTS=`getopt -o u:c:l: -l url:,converter:,last-modified: -- "$@"`
   if [ $? -ne 0 ]; then
     return 1
@@ -36,21 +43,24 @@ load_params() {
     esac
   done
 
-  # オプション確認
-  if [ -z $DOWNLOAD_URL ]; then
+  # Option validation
+  if [ -z "$DOWNLOAD_URL" ]; then
     TEMP="$REQUIRE_OPTS --url"
     REQUIRE_OPTS="$TEMP"
   fi
-  if [ -z $HTTP_HEADER_CONV ]; then
+  if [ -z "$HTTP_HEADER_CONV" ]; then
     TEMP="$REQUIRE_OPTS --converter"
     REQUIRE_OPTS="$TEMP"
+  elif [ ! -e "$HTTP_HEADER_CONV" ]; then
+    log_error "'$HTTP_HEADER_CONV' is not exist."
+    return 1
   fi
-  if [ -z $LAST_MODIFIED_STORE ]; then
+  if [ -z "$LAST_MODIFIED_STORE" ]; then
     TEMP="$REQUIRE_OPTS --last-modified"
     REQUIRE_OPTS="$TEMP"
   fi
   if [ -n "$REQUIRE_OPTS" ]; then
-    echo "Require options:\n $REQUIRE_OPTS"
+    log_error "Require options:\n $REQUIRE_OPTS"
     return 1
   fi
   return 0
@@ -59,6 +69,7 @@ load_params() {
 check_update() {
   local HEADER RAW CODE
 
+  # Request to Web
   if [ -n "$2" ]; then
     HEADER="If-Modified-Since: \"$2\""
     RAW=`curl "$1" --head --header "$HEADER"`
@@ -66,82 +77,87 @@ check_update() {
     RAW=`curl "$1" --head`
   fi
   if [ $? -ne 0 ]; then
-    echo 'Invalid argument.'
+    log_error 'Invalid argument.'
     return 2
   fi
+
+  # Analyze response from the web
   RESPONSE=`echo "$RAW" | $3`
   CODE=`echo "$RESPONSE" | jq -e -r ".code" | bc`
   if [ $? -ne 0 ]; then
-    echo "HTTP Status code is not in response."
+    log_error "HTTP Status code is not in response."
     return 2
   elif [ $CODE -eq 304 ]; then
-    echo 'Content is not updated.'
+    log_error 'Content is not updated.'
     return 1
   elif [ $CODE -ne 200 ]; then
-    echo 'Can not download.'
+    log_error 'Can not download.'
     return 2
   fi
   return 0
 }
 
 download() {
-  echo "Start downloading from '$1'."
+  log_info "Start downloading from '$1'."
   wget "$1" | bc
   if [ $? -ne 0 ]; then
-    echo 'Can not download.'
+    log_error 'Can not download.'
     return 1
   fi
-  echo 'Download completed.'
+  log_info 'Download completed.'
   return 0
 }
 
 load_last_modified() {
-  echo "Last Modified read from '$1'."
-  LAST_MODIFIED=`cat $LAST_MODIFIED_STORE | tr -d "\r" | tr -d "\n"`
+  if [ ! -e "$1" ]; then
+    LAST_MODIFIED=
+    return 1
+  fi
+
+  log_info "Last Modified read from '$1'."
+  LAST_MODIFIED=`cat $1 | tr -d "\r" | tr -d "\n"`
   if [ $? -ne 0 ]; then
     LAST_MODIFIED=
     return 1
   fi
   return 0
-  echo "Done."
+  log_info "Done."
 }
 
 save_last_modified() {
   local LAST_MODIFIED
 
-  echo "Last Modified write to '$2'."
+  log_info "Last Modified write to '$2'."
   LAST_MODIFIED=`echo "$1" | jq -e -r ".lastModified"`
   if [ $? -ne 0 ]; then
-    echo 'Last modified is not in response.'
+    log_error 'Last modified is not in response.'
     return 1
   fi
   echo "$LAST_MODIFIED" > "$2"
   if [ $? -ne 0 ]; then
-    echo "Write failed."
+    log_error "Write failed."
     return 1
   fi
-  echo "Done."
+  log_info "Done."
   return 0
 }
 
-# オプション取得
 load_params "$@"
 if  [ $? -ne 0 ]; then
-  cat << EOT
+  USAGE=`cat << EOT
 Usage:
-  download.sh <--url URL> <--converter Converter> <-- --last-modified Path>
+  $SHELL_PATH <--url URL> <--converter Converter> <-- --last-modified Path>
 Options:
   url           Download URL
   converter     HTTP Response Header Converter
   last-modified Content last modified store path
-EOT
+EOT`
+  log_error "$USAGE"
   exit 1
 fi
 
-# 最終更新日時読み込み
 load_last_modified "$LAST_MODIFIED_STORE"
 
-# 更新確認
 check_update "$DOWNLOAD_URL" "$LAST_MODIFIED" "$HTTP_HEADER_CONV"
 if  [ $? -eq 1 ]; then
   exit 0
@@ -149,12 +165,10 @@ elif [ $? -eq 0 ]; then
   exit 0
 fi
 
-# ダウンロード
 download "$DOWNLOAD_URL"
 if  [ $? -ne 0 ]; then
   exit 1
 fi
 
-# 最終更新日時保存
 save_last_modified "$RESPONSE" "$LAST_MODIFIED_STORE"
 exit 0
